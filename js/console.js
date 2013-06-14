1bff4b046fec35a98cbc5ec0e7bb71b865d26c9b
DBProductions <dennis@dbproductions.de>
2013-06-15 01:49:16 +0800

    init commit with first files

---
+++ js/console.js
@@ -0,0 +1,138 @@
+var shell = document.querySelector('#shell');
+var lastCommands = [];
+var commandCount = 0;
+var inputPossible = true;
+var interpreter = new Interpreter(shell);
+/**
+ * init shell
+ */
+window.onload = function() {
+    shell.value = "";
+    shell.value = ">";
+    shell.selectionStart = 1;
+    shell.focus();
+};
+/**
+ * dirty hack to scroll textarea down all the time
+ */ 
+setInterval(function() {
+    shell.scrollTop = shell.scrollHeight;
+}, 100);
+/**
+ * helper functions
+ */
+function insertAtCursor(myField, myValue) {
+    // IE support
+    if (document.selection) {
+        myField.focus();
+        sel = document.selection.createRange();
+        sel.text = myValue;
+    }
+    // MOZILLA and others
+    else if (myField.selectionStart || myField.selectionStart == '0') {
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+        myField.value = myField.value.substring(0, startPos) + myValue + myField.value.substring(endPos, myField.value.length);
+    } else {
+        myField.value += myValue;                
+    }
+}
+/**
+ * handle key down
+ * left - go left to attend to '>'
+ * up - increment commandCount and display last commands
+ * down - decrement commandCount and display last commands
+ * enter - get command string, push it in lastCommands and set commandCount, pass command to interpreter
+ */
+shell.addEventListener('keydown', function(e) {
+    if (!inputPossible) {
+        e.preventDefault();
+        return false;
+    }
+    /** 
+     * left key
+     */
+    if (e.keyCode === 37) {
+        var completeLength = shell.value.length;
+        var currentPos = shell.selectionStart;
+        var lines = shell.value.split("\n");
+        if (lines[lines.length-1] === '>') {
+            e.preventDefault();
+            return false;
+        } else if((lines[lines.length-1].length - (completeLength-currentPos)) === 1) {
+            e.preventDefault();
+            return false;
+        }            	
+    /**
+     * up key
+     */
+    } else if (e.keyCode === 38) {
+        e.preventDefault(); 
+        if (lastCommands[commandCount]) {
+            var lines = shell.value.split("\n");
+            lines[lines.length-1] = '>' + lastCommands[commandCount];
+            shell.value = lines.join("\n");
+        }                
+        if (commandCount > 0) commandCount--;
+        return false;
+    /**
+     * down key
+     */
+    } else if (e.keyCode === 40) {
+        e.preventDefault();
+        if (commandCount < lastCommands.length) commandCount++;
+        var lines = shell.value.split("\n");
+        if (lastCommands[commandCount]) {
+            lines[lines.length-1] = '>' + lastCommands[commandCount];	
+        } else {
+            lines[lines.length-1] = '>';
+        }
+        shell.value = lines.join("\n");
+        return false;
+    /**
+     * enter key
+     */
+    } else if (e.keyCode === 13) {
+        var lines = shell.value.split("\n");
+        var commandStr = lines[lines.length-1].slice(1);                
+        if (lines[lines.length-1] !== '' && commandStr !== '') {
+            lastCommands.push(commandStr);
+            commandCount = lastCommands.length - 1;
+            inputPossible = false;
+            interpreter.handleCommand(commandStr, function() {                
+                inputPossible = true;
+                var lines = shell.value.split("\n");
+                if (lines[lines.length-1][0] === undefined) {
+                    insertAtCursor(shell, '>');            
+                }
+            });            
+        }
+        return false;
+    }
+}, true);
+/**
+ * handle keyup
+ * enter - insert '>' at the begin of line
+ * back - insert '>' when deleted
+ */
+shell.addEventListener('keyup', function(e) {
+    if (!inputPossible) {
+        e.preventDefault();
+        return false;
+    }
+    /** 
+     * enter key
+     */
+    if (e.keyCode === 13) {
+        insertAtCursor(shell, '>');
+    }
+    /** 
+     * back key
+     */
+    if (e.keyCode === 8) {
+        var lines = shell.value.split("\n");
+        if (lines[lines.length-1] === '') {
+            insertAtCursor(shell, '>');
+        }
+    }
+}, true);
---
+++ js/interpreter.js
@@ -0,0 +1,111 @@
+/**
+ * very simple interpreter
+ */
+function Interpreter(shell) {
+    this.shell = shell;
+    /**
+     * define callable functions with or without return values
+     */
+    this.rocks = function rocks(cb) {                
+        cb('javascript rocks!!! 就是就是');
+    }; 
+    this.make = function make(cb, args) {
+        if(args) {
+            if (args[0] === '-help' || args[0] === '-h') {
+                var helpStr = "\nmake is a test command\nit only display the given arguments and this text";
+                this.shell.value += helpStr;
+            } else {
+                var content = this.shell.value;
+                for(var i = 0; i < args.length; i++) { 
+                    content += "\n" + i + ": " + args[i]; 
+                }
+                content += "\nreturn: " + doCall(args[0]);
+                this.shell.value = content;                
+            }
+        } else {
+            this.shell.value += "\nno arguments, type make -h or make -help";
+        }
+        cb(null);
+    };
+    this.timer = function timer(cb) {
+        var count = 0;
+        var inter = setInterval(function() {
+            count += 1;
+            if (count === 5) {
+                clearInterval(inter);
+            }
+            this.shell.value += "step " + (20*count) + "%\n";
+        }, 1000);
+        setTimeout(function() {
+            cb(null);
+        }, 5010);        
+    };
+    this.install = function install(cb) {
+        var lines = this.shell.value;
+        for(var i = 0; i < 10000; i++) { lines += "\n" + '-- loading environment step: ' + i; }
+        this.shell.value = lines;
+        setTimeout(function() {
+            cb(null);
+        }, 1000);        
+    };
+    this.clear = function clear(cb) {
+        this.shell.value = "";
+        this.shell.value = ">";
+        this.shell.selectionStart = 1;
+        this.shell.focus();
+        cb(null);
+    };
+    /**
+     * display defined functions
+     *
+     * TODO:
+     * register functions from other objects and display them
+     */
+    this.help = function help(cb) {
+        var lines = this.shell.value;
+        for(i in this) {
+            if (typeof this[i] === 'function' && this[i].name !== 'handleCommand' && this[i].name !== 'help') {
+                lines += "\n" + this[i].name;                
+            }
+        }                
+        this.shell.value = lines;
+        cb();
+    };
+    /**
+     * seperate command from args
+     * handle specific command 
+     * - call when present and is from type function
+     *
+     * TODO:
+     * deligate to other objects and act as a facade for the console
+     */
+    this.handleCommand = function handleCommand(command, cb) {
+        var commandArr = command.split(' ');
+        var command = commandArr[0];
+        var args = null;
+        if (commandArr.length > 1) {
+            args = commandArr.slice(1); 
+        }
+        if (typeof this[command] === 'function' && command !== 'handleCommand') {
+            this[command](function (response) {
+                if (response) {
+                    this.shell.value += "\n" + response;
+                }
+                cb();                    
+            }, args);
+        } else {
+            this.shell.value += "\n'" + command + "' not found, type help to see commands";
+            cb();
+        }
+    };
+}
+/**
+ * do fake call with sinon
+ */
+function doCall(arg) {
+    var callback = sinon.stub();
+    callback.returns(5);
+    callback.withArgs(1).returns(10);
+    callback.withArgs('install').returns('start to install...finish');
+    return callback(arg);
+}
